4bf2717c527e325ec7afd5b1a3d871e38481504d
florlihuel <florenciabelenbalderramas@gmail.com>
2022-08-14 15:15:17 -0300

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>¡HOLA MUNDO! </h1>
+        <h2>Futura tienda de insumos de tatto. </h2>
+
+
+ 
+      </header>
+    </div>
+  );
+}
+
+export default App;
